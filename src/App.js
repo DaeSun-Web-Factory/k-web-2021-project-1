9d7c6c3ca135ff3b7fae5546d6588e987c0b7fb5
SongDaeSun <backgom07@gmail.com>
2021-11-13 13:29:10 +0900

    done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 
 import { Auth, Hub } from 'aws-amplify';
 
-import { TextField, Button, Typography, Paper, Container, Grid } from '@material-ui/core';
+import { TextField, Button, Typography, Paper, Container, Grid, CircularProgress, Card} from '@material-ui/core';
+
+import HouseIcon from '@material-ui/icons/House';
+import LocalPhoneIcon from '@material-ui/icons/LocalPhone';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
+import axios from 'axios';
 
 import useStyles from './style';
-
-import {listUsers} from './graphql/queries';
-import {createUser} from './graphql/mutations';
 
 
 const initialFormState = {
@@ -22,13 +26,14 @@ const selectUser = {
 function App() {
 
     const [formState, updateFormState] = useState(initialFormState)
-	const [user, updateUser] = useState(null)
-	const [selectedInfo, updateSelectedInfo] = useState(selectUser)
+	const [currentUser, updateCurrentUser] = useState()
+	const [userInfo, updateUserInfo] = useState(null)
 
 	useEffect(() => {
 		checkUser()
 		setAuthListener()
-	}, [])
+	}, []);
+
 
 
 	const classes = useStyles();
@@ -36,13 +41,9 @@ function App() {
 	async function setAuthListener() {
 		Hub.listen('auth', (data) => {
 			switch (data.payload.event) {
-				case 'signIn':
-					console.log('user signed in');
-					break;
 				
 				case 'signOut':
 					updateFormState(() => ({ ...formState, formType: "signIn" }))
-					console.log('user signed out');
 					break;
 				
 				default:
@@ -54,22 +55,76 @@ function App() {
 	async function checkUser() {
 		try {
 			const user = await Auth.currentAuthenticatedUser()
-			updateUser(user)
+
+			let currentUserName = user.username
+			updateCurrentUser(currentUserName)
+
 			updateFormState(() => ({ ...formState, formType: "signedIn" }))
 
+			fetchUserData()
+
 		} catch (err){
-			updateUser(null)
+			//updateUser(null)
 			updateFormState(() => ({ ...formState, formType: "signIn" }))
 		}
 	}
 
-	function onChange(e){
-		e.persist()
-		updateFormState(() => ({ ...formState, [e.target.name]: e.target.value }))
-	}
 
 	const { formType } = formState
 
+	
+	const createNewUserData = async() => {
+		const { username, email, name, address, phone  } = formState
+
+		const newUserDataInput = {
+			"ID": username,
+			"name": name,
+			"email": email,
+			"address": address,
+			"phone": phone
+		}
+
+		var myHeaders = new Headers();
+
+		myHeaders.append("Content-Type", "application/json");
+
+		var raw = JSON.stringify(newUserDataInput);
+		// create a JSON object with parameters for API call and store in a variable
+		var requestOptions = {
+			method: 'POST',
+			headers: myHeaders,
+			body: raw,
+			redirect: 'follow'
+		};
+		// make API call with parameters and use promises to get response
+		fetch("https://3bv0uv3wxi.execute-api.ap-northeast-2.amazonaws.com/dev", requestOptions)
+	}
+
+	
+
+	async function fetchUserData() {
+
+		try {
+			const response = await axios.get(
+				'https://3bv0uv3wxi.execute-api.ap-northeast-2.amazonaws.com/dev/'
+			);
+
+			let userDataList = response.data.data.Items;
+
+
+			updateUserInfo(userDataList);
+		
+			
+			return true;
+
+		} catch (e) {
+			console.log(e);
+			return TextTrackCueList;
+		}
+	}
+
+
+	 
 
 	async function signUp(e) {
 		e.preventDefault();
@@ -83,14 +138,19 @@ function App() {
 		const { username, email, password } = formState
 		await Auth.signUp({ username, password, attributes: { email }})
 		updateFormState(() => ({ ...formState, formType: "confirmSignUp" }))
+
+		
 	}
 
+	
 	async function confirmSignUp(e) {
 		e.preventDefault();
 
 		const { username, authCode } = formState
 		await Auth.confirmSignUp(username, authCode)
 		updateFormState(() => ({ ...formState, formType: "signIn" }))
+
+		await createNewUserData()
 	}
 
 	async function signIn(e) {
@@ -99,6 +159,8 @@ function App() {
 		const { username, password } = formState
 		await Auth.signIn(username, password)
 		updateFormState(() => ({ ...formState, formType: "signedIn" }))
+
+		fetchUserData()
 	}
 
 	async function goToSignUp(e){
@@ -108,7 +170,7 @@ function App() {
 
     return (
 		<Container className="App" maxidth="lg">
-			<Grid container alignItems="stretch" spacing={3} justify="center">
+			<Grid container alignItems="stretch" spacing={3} justifyContent="center">
 			{
 				formType === 'signUp' && (
 					<Paper className={classes.paper}>
@@ -308,9 +370,51 @@ function App() {
 			{
 				formType === 'signedIn' && (
 					<div>
-						<h1>Hello world, welcome user!</h1>
-						<button onClick={() => Auth.signOut()}> Sign Out </button>
+						<h1>
+							Hello world, welcome users! 
+							<Button onClick={() => Auth.signOut()}> 
+								Sign Out
+							</Button>
+						</h1>
+						
+						{!userInfo ? <CircularProgress/> : 
+							<Grid className={classes.container} container alignItems="stretch" spacing={3}>
+								{userInfo.map((userIn, index) => (
+									<Grid key={index} item xs={12} sm={12}>
+										<Card className={classes.card} >
+
+											<Button size="large" style={{textTransform: 'none'}}>
+												<AccountCircleIcon className={classes.icons} fontSize="medium" />
+												<Typography variant="h6"> {userIn.ID+" ("+userIn.name+")"}</Typography>
+											</Button>
+
+											<Button size="large" style={{textTransform: 'none'}}>
+												<MailOutlineIcon className={classes.icons} fontSize="medium" />
+												<Typography variant="body1"> {userIn.email}</Typography>
+											</Button>
+
+											<Button size="large" style={{textTransform: 'none'}}>
+												<LocalPhoneIcon className={classes.icons} fontSize="medium" />
+												<Typography variant="body1"> {userIn.phone}</Typography>
+											</Button>
+
+											<Button size="large" style={{textTransform: 'none'}}>
+												<HouseIcon className={classes.icons} fontSize="medium" />
+												<Typography variant="body1"> {userIn.address}</Typography>
+													
+											</Button>
+
+											
+
+										</Card>
+									</Grid>
+								))}
+							</Grid>
+							
+						}
+						
 					</div>
+					
 				)
 			}
 			</Grid>
--- src/style.js
+++ src/style.js
@@ -26,4 +26,63 @@ export default makeStyles((theme) => ({
         textDecoration: 'none',
         color: 'white'
     },
+    media: {
+        height: 0,
+        paddingTop: '56.25%',
+        marginTop:'30',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        backgroundBlendMode: 'darken',
+    },
+    border: {
+        border: 'solid',
+    },
+    fullHeightCard: {
+        height: '100%',
+    },
+    card: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        borderRadius: '15px',
+        height: '100%',
+        position: 'relative',
+    },
+    overlay: {
+        position: 'absolute',
+        top: '20px',
+        left: '20px',
+        color: 'white',
+    },
+    overlay2: {
+        position: 'absolute',
+        top: '20px',
+        right: '20px',
+        color: 'white',
+    },
+    grid: {
+        display: 'flex',
+    },
+    detailsTop: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        marginLeft: '20px',
+        marginTop: '20px',
+    },
+    detailsBottom: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        marginLeft: '20px',
+        marginBottom: '20px',
+    },
+    title: {
+        padding: '0 16px',
+    },
+    cardActions: {
+        padding: '0 16px 8px 16px',
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+    icons: {
+        marginRight: '2%',
+    }
 }));
